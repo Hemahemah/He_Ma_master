6d2d23dc4a1e2b0f92ba0a3c286c9c6a1ce5b0a4
zlh <2818323885@qq.com>
2022-08-12 01:14:58 +0800

    修改细节

--- src/main/java/com/zlh/he_ma_master/config/HeMaExecutorConfigurer.java
+++ src/main/java/com/zlh/he_ma_master/config/HeMaExecutorConfigurer.java
@@ -16,31 +16,6 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class HeMaExecutorConfigurer {
 
-
-    @Resource
-    private IndexThreadPoolConfig indexThreadPoolConfig;
-
-    @Bean
-    public Executor indexConfigServiceExecutor(){
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        //配置核心线程数
-        executor.setCorePoolSize(indexThreadPoolConfig.getCorePoolSize());
-        //配置最大线程数
-        executor.setMaxPoolSize(indexThreadPoolConfig.getMaxPoolSize());
-        //配置队列大小
-        executor.setQueueCapacity(indexThreadPoolConfig.getQueueCapacity());
-        //配置活跃时间
-        executor.setKeepAliveSeconds(indexThreadPoolConfig.getKeepAliveSeconds());
-        //配置线程池中的线程的名称前缀
-        executor.setThreadNamePrefix("indexConfig-service-");
-        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        //执行初始化
-        executor.initialize();
-        return executor;
-    }
-
     @Bean
     public Executor delayServiceExecutor(){
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
